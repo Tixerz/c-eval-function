a9db477802eba792b2cc273f3b71ac4f20c129af
Tixerz <petete1296@gmail.com>
2023-06-11 20:18:01 +0330

    eval

---
+++ apply.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int apply_op(int num1, int num2, char op) {
+	switch (op) {
+		case '*':
+			return (num1 * num2);
+			break;
+		case '/':
+			return (num1 / num2);
+			break;
+		case '+':
+			return (num1 + num2);
+			break;
+		case '-':
+			return (num1 - num2);
+			break;
+
+	}
+	return 0;
+}
+
