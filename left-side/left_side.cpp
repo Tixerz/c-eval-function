820e04f70c244bd1b7bccc46577fc048ebd465e3
Tixerz <petete1296@gmail.com>
2023-06-12 19:14:28 +0330

    left and right side number finders

--- left-side/left_side.cpp
+++ left-side/left_side.cpp
@@ -2,22 +2,34 @@
 #include <string>
 using namespace std ; 
 int start1; 
-int left_side( string str, int op_index){
-    string output1 ; 
-    string output2 ; 
-    for(int i = op_index-1 ; i >= 0 ; i--){
-        if (str[i] == '/' || str[i] == '*' || str[i] == '+' || str[i] == '-' ){
+int left_side(string str, int op_index) {
+    string output_real; 
+    int stop = 0; 
+    string output1;
+    string output2;
+    for (int i = op_index - 1; i >= 0; i--) {
+        if (str[i] == '/' || str[i] == '*' || str[i] == '+') {
             start1 = i;
-            break ; 
+            break;
         }
-        output1 = output1 + str[i] ; 
-        
+        output1 = output1 + str[i];
+
     }
     //reverse the output
-    for (int i = output1.length()- 1 ; i >= 0 ; i--){
-        output2 += output1[i] ;
+    for (int i = output1.length() ; i >= 0; i--) {
+        output2 += output1[i];
     }
-    int num = stoi(output2) ;
+    for (int x = output2.length(); x >= 0; x--) {
+        if (output2[x] != '-') {
+            output_real.push_back(output2[x]);
+        }
+        if (output2[x] == '-') {
+            output_real.insert(0, "-");
+            break;
+        }
+    }
+    
+    int num = stoi(output_real);
     return num;
 }
 
