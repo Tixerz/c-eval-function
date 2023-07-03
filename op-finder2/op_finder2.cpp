aedf18e388550d20348a101a2ffc5217b9806a2c
Tixerz <petete1296@gmail.com>
2023-07-03 20:27:16 +0330

    first negative number acception

--- op-finder2/op_finder2.cpp
+++ op-finder2/op_finder2.cpp
@@ -6,7 +6,7 @@ string op2;
 void op_finder2(string str){
 
     for(int i=0 ; i<str.length();i++){
-        if(str[i] == '+' || str[i] == '-'){
+        if(str[i] == '+' || (str[i] == '-' && i !=0)){
             op2 = str[i] ;
             op2_index = i ;
             break;
